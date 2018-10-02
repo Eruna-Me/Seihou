b1ab53fb634dac3e42e7f3917ca76cd3162d53de
Hidde Bartelink <hiddetelink@live.nl>
2018-10-02 14:47:23 +0200

    Various stuff

---
+++ Seihou/Seihou/Enemies/YukiOnna.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Seihou
+{
+	class YukiOnna : Enemy
+	{
+		private const float fallSpeed = 100.0f;
+		private const float bulletSpeed = 400.0f;
+		private float fireTimer = 2f;
+		private float fireDelay = 0f;
+		private const float maxFireDelay = 0.1f;
+		private int ammo = 5;
+		private bool targetSet = false;
+		private Vector2 target;
+		private const float spread = 20;
+
+		public YukiOnna(Vector2 pos, SpriteBatch sb, EntityManager em) : base(pos, sb, em)
+		{
+			texture = "Samurai";
+			size = 10;
+			speed.Y = fallSpeed;
+			hp = 20;
+		}
+
+		public override void Update(GameTime gt)
+		{
+			base.Update(gt);
+			pos += speed * (float)gt.ElapsedGameTime.TotalSeconds;
+			if (fireDelay <= 0 && ammo > 0 && fireTimer <= 0)
+			{
+				if (!targetSet)
+				{
+					target = Global.Normalize(em.GetPlayer().pos - pos) * bulletSpeed;
+					targetSet = true;
+				}
+				float Direction = Global.VtoD(target);
+				em.AddEntity(new EnemyBullet(pos, sb, em, this, new Vector2((float)Math.Cos(Direction + Math.PI / spread) *bulletSpeed, (float)Math.Sin(Direction + Math.PI / spread)* bulletSpeed)));
+				em.AddEntity(new EnemyBullet(pos, sb, em, this, new Vector2((float)Math.Cos(Direction + Math.PI / (spread/2)) * bulletSpeed, (float)Math.Sin(Direction + Math.PI / (spread / 2)) * bulletSpeed)));
+				em.AddEntity(new EnemyBullet(pos, sb, em, this, new Vector2((float)Math.Cos(Direction - Math.PI / spread) * bulletSpeed, (float)Math.Sin(Direction - Math.PI / spread) * bulletSpeed)));
+				em.AddEntity(new EnemyBullet(pos, sb, em, this, new Vector2((float)Math.Cos(Direction - Math.PI / (spread/2)) * bulletSpeed, (float)Math.Sin(Direction - Math.PI / (spread / 2)) * bulletSpeed)));
+
+				em.AddEntity(new EnemyBullet(pos, sb, em, this, target));
+				fireDelay = maxFireDelay;
+				ammo--;
+			}
+
+			fireDelay -= 1 * (float)gt.ElapsedGameTime.TotalSeconds;
+			fireTimer -= 1 * (float)gt.ElapsedGameTime.TotalSeconds;
+		}
+	}
+}
