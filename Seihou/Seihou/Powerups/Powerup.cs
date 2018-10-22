85820eff5d356e1c30286d6595845c49185fc424
Hidde Bartelink <hiddetelink@live.nl>
2018-10-22 12:17:13 +0200

    Powerups are now homing

--- Seihou/Seihou/Powerups/Powerup.cs
+++ Seihou/Seihou/Powerups/Powerup.cs
@@ -17,22 +17,33 @@ namespace Seihou
 		private const float xDeceleration1 = 50;
 		private const float xDeceleration2 = 200;
 		private const float xDeceleration2Margin = 75; //this has to be the worst variable name in the entire project
+		private const int homingRange = 64;
+		private const float homingSlowness = 15;
+		private const float homingSpeed = 200.0f;
 
 		public Powerup(Vector2 pos, SpriteBatch sb, EntityManager em) : base(pos, sb, em)
 		{
-			size = 25;
+			size = 12;
 			speed.Y = -50;
 			speed.X = Global.random.Next(-deltaXspeed, deltaXspeed);
 		}
 
 		public override void Update(GameTime gt)
 		{
-			speed.Y += acceleration * (float)gt.ElapsedGameTime.TotalSeconds;
+			Entity h = em.Touching(pos, homingRange, EntityManager.EntityClass.player);
+			if (h != null)
+			{
+				speed = (Global.Normalize(Global.player.pos - pos) * homingSpeed + speed * homingSlowness) / (homingSlowness + 1);
+			}
+			else
+			{ 
+				speed.Y += acceleration * (float)gt.ElapsedGameTime.TotalSeconds;
 
-            if(speed.X > 0)	speed.X -= (speed.X > xDeceleration2Margin ? xDeceleration2 : xDeceleration1) * (float)gt.ElapsedGameTime.TotalSeconds; 
-            if(speed.X < 0)	speed.X += (speed.X < xDeceleration2Margin ? xDeceleration2 : xDeceleration1) * (float)gt.ElapsedGameTime.TotalSeconds; 
+				if (speed.X > 0) speed.X -= (speed.X > xDeceleration2Margin ? xDeceleration2 : xDeceleration1) * (float)gt.ElapsedGameTime.TotalSeconds;
+				if (speed.X < 0) speed.X += (speed.X < xDeceleration2Margin ? xDeceleration2 : xDeceleration1) * (float)gt.ElapsedGameTime.TotalSeconds;
+			}
 
-            if (speed.Y > maxSpeed) speed.Y = maxSpeed;
+			if (speed.Y > maxSpeed) speed.Y = maxSpeed;
 
 			pos += speed * (float)gt.ElapsedGameTime.TotalSeconds;
 
