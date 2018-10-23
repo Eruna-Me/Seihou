17c7546c43505816abcaceaf91adee6f0cc7d79c
Hidde Bartelink <hiddetelink@live.nl>
2018-10-23 11:28:14 +0200

    Cloud speed is now variable
    And clouds now get deleted when out of screen.

--- Seihou/Seihou/Graphics/Cloud.cs
+++ Seihou/Seihou/Graphics/Cloud.cs
@@ -12,16 +12,23 @@ namespace Seihou
 	class Cloud : Entity
 	{
 		private float alpha;
+		private float deltaSpeed;
 
-		public Cloud(Vector2 pos, SpriteBatch sb, EntityManager em, String texture, float alpha) : base(pos, sb, em)
+		public Cloud(Vector2 pos, SpriteBatch sb, EntityManager em, String texture, float alpha, float deltaSpeed) : base(pos, sb, em)
 		{
 			ec = EntityManager.EntityClass.cloud;
 			this.alpha = alpha;
+			this.deltaSpeed = deltaSpeed;
 		}
 
 		public override void Update(GameTime gt)
 		{
-			pos += CloudManager.speed * (float)gt.ElapsedGameTime.TotalSeconds;
+			pos.Y += (CloudManager.speed + deltaSpeed) * (float)gt.ElapsedGameTime.TotalSeconds;
+
+			if (pos.Y > Global.screenHeight + Global.outOfScreenMargin)
+			{
+				em.RemoveEntity(this);
+			}
 		}
 
 		public override void Draw(GameTime gt)
--- Seihou/Seihou/Graphics/CloudManager.cs
+++ Seihou/Seihou/Graphics/CloudManager.cs
@@ -13,17 +13,19 @@ namespace Seihou
 	static class CloudManager
 	{
 		static public Color color = Color.White;
-		static public Vector2 speed = new Vector2(0,100);
+		static public float speed = 100.0f;
 		static public float minAlpha = 0.0f;
 		static public float maxAlpha = 1.0f;
 		static public float spawnChance = 1.0f;
+		static public float deltaSpeedVariance = 20.0f;
 
 		public static void Update(GameTime gt, SpriteBatch sb, EntityManager em)
 		{	
 			Vector2 pos = new Vector2(Global.random.Next(-Global.outOfScreenMargin, Global.playingFieldWidth + Global.outOfScreenMargin),Global.spawnHeight);
 			float alpha = (float)Global.random.NextDouble() * (maxAlpha - minAlpha) - minAlpha;
+			float deltaSpeed = (float)Global.random.NextDouble() * deltaSpeedVariance;
 
-			em.AddEntity(new Cloud(pos, sb, em, "Cloud1", alpha));
+			em.AddEntity(new Cloud(pos, sb, em, "Cloud1", alpha, deltaSpeed));
 		}
 	}
 }
