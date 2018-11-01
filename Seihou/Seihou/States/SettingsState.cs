7f1f0f457cc0952a635995e47c94cc41952c1eb1
Dennnnis <dennisbeckertim@gmail.com>
2018-11-01 10:42:02 +0100

    Added settingsState class
    so that hidde can't edit the csproj
    and make my life very difficult

---
+++ Seihou/Seihou/States/SettingsState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Seihou
+{
+	class SettingsState : State
+	{
+		public SettingsState(StateManager sm, ContentManager cm, SpriteBatch sb, GraphicsDeviceManager gdm, double score, string mode) : base(sm, cm, sb, gdm)
+		{
+
+		}
+
+		public override void Draw(GameTime gt)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Update(GameTime gt)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
