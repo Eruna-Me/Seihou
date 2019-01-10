0e5bfbfac2c7731d6470faa57d0100896d5b4494
Dennnnis <dennisbeckertim@gmail.com>
2019-01-10 15:07:39 +0100

    Update EntityManager.cs
    Its now even more complex....

--- Seihou/Seihou/Main/EntityManager.cs
+++ Seihou/Seihou/Main/EntityManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using System.Linq;
 
 namespace Seihou
 {
@@ -7,7 +8,7 @@ namespace Seihou
     {
         public enum EntityClass
         {
-			cloud = 0,
+			cloud,
             nonSolid,
 			enemyProjectile,
 			enemy,
@@ -15,123 +16,102 @@ namespace Seihou
             ui,
         }
 
-        private readonly Dictionary<EntityClass, List<Entity>>  entities =           new Dictionary<EntityClass, List<Entity>>();
-        private readonly Dictionary<EntityClass, Queue<Entity>> pollAddEntities =    new Dictionary<EntityClass, Queue<Entity>>();
-        private readonly Dictionary<EntityClass, Queue<Entity>> pollRemoveEntities = new Dictionary<EntityClass, Queue<Entity>>();
+		public enum Collections
+		{
+			Entities,
+			PollAdd,
+			PollRemove,
+		}
+
+		public Dictionary<Collections, Dictionary<EntityClass,List<Entity>>> EntityCollections = new Dictionary<Collections, Dictionary<EntityClass, List<Entity>>>()
+		{
+			{Collections.Entities, new Dictionary<EntityClass, List<Entity>>() },
+			{Collections.PollAdd, new Dictionary<EntityClass, List<Entity>>() },
+			{Collections.PollRemove, new Dictionary<EntityClass, List<Entity>>() },
+		};
 
         public EntityManager()
         {
-			entities.Add(EntityClass.cloud,				 new List<Entity>());
-			entities.Add(EntityClass.nonSolid,           new List<Entity>());
-            entities.Add(EntityClass.enemy,              new List<Entity>());
-            entities.Add(EntityClass.player,             new List<Entity>());
-            entities.Add(EntityClass.ui,                 new List<Entity>());
-			entities.Add(EntityClass.enemyProjectile,	 new List<Entity>());
-
-			pollAddEntities.Add(EntityClass.cloud,		 new Queue<Entity>());
-			pollAddEntities.Add(EntityClass.nonSolid,    new Queue<Entity>());
-            pollAddEntities.Add(EntityClass.enemy,       new Queue<Entity>());
-            pollAddEntities.Add(EntityClass.player,      new Queue<Entity>());
-            pollAddEntities.Add(EntityClass.ui,          new Queue<Entity>());
-			pollAddEntities.Add(EntityClass.enemyProjectile, new Queue<Entity>());
-
-			pollRemoveEntities.Add(EntityClass.cloud,	 new Queue<Entity>());
-			pollRemoveEntities.Add(EntityClass.nonSolid, new Queue<Entity>());
-            pollRemoveEntities.Add(EntityClass.enemy,    new Queue<Entity>());
-            pollRemoveEntities.Add(EntityClass.player,   new Queue<Entity>());
-            pollRemoveEntities.Add(EntityClass.ui,       new Queue<Entity>());
-			pollRemoveEntities.Add(EntityClass.enemyProjectile, new Queue<Entity>());
+			foreach (var i in EntityCollections.Values) foreach (var p in System.Enum.GetValues(typeof(EntityClass))) i.Add((EntityClass)p,new List<Entity>());
 		}
-    
-        public Entity GetPlayer() => entities[EntityClass.player][0];
+
+		public Entity GetPlayer() => EntityCollections[Collections.Entities][EntityClass.player][0];
 
         public Entity FindById(int id)
         {
-            foreach (KeyValuePair<EntityClass, List<Entity>> pair in entities)
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.Entities])
             {
                 foreach (Entity e in pair.Value)
-                {
-                    if (e.id == id)
-                    {
-                        return e;
-                    }
-                }
+					if (e.id == id) return e;
             }
             return null;
         }
 
         public Entity Touching(Entity ent,EntityClass ec)
         {
-            foreach (Entity e in entities[ec])
+            foreach (Entity e in EntityCollections[Collections.Entities][ec])
             {
                 if (e.id == ent.id) continue;
-                if (Collision.Circle(ent,e))
-                {
-                    return e;
-                }
+                if (Collision.Circle(ent,e)) return e;
             }
             return null;
         }
 
         public Entity Touching(Vector2 pos, int size, EntityClass ec)
         {
-            foreach (Entity e in entities[ec])
-            {
-                if (Collision.Circle(e,pos,size))
-                {
-                    return e;
-                }
-            }
+            foreach (Entity e in EntityCollections[Collections.Entities][ec])
+                if (Collision.Circle(e,pos,size)) return e;
             return null;
         }
 
-        public List<Entity> GetEntities(EntityClass e) => entities[e];
+        public List<Entity> GetEntities(EntityClass e) => EntityCollections[Collections.Entities][e];
 
         public void DamageAll(int damage,EntityClass ec)
         {
-            foreach (Entity e in entities[ec])
-            {
-                e.OnDamaged(null, damage);
-            }
+            foreach (Entity e in EntityCollections[Collections.Entities][ec]) e.OnDamaged(null, damage);
         }
 
         public int GetEntityCount()
         {
             int c = 0;
-            foreach (KeyValuePair<EntityClass, List<Entity>> pair in entities)
-                c += pair.Value.Count;
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.Entities])
+				c += pair.Value.Count;
             return c;
         }
 
-        public int GetEntityCount(EntityClass e) => entities[e].Count;
+        public int GetEntityCount(EntityClass e) => EntityCollections[Collections.Entities][e].Count;
 
-        public void RemoveEntity(Entity ent) => pollRemoveEntities[ent.ec].Enqueue(ent);
+		public void RemoveEntity(Entity ent) => EntityCollections[Collections.PollRemove][ent.ec].Add(ent);
 
-        public void AddEntity(Entity ent) => pollAddEntities[ent.ec].Enqueue(ent);
+		public void AddEntity(Entity ent) => EntityCollections[Collections.PollAdd][ent.ec].Add(ent);
 
         public void Update(GameTime gt)
         {
-            foreach (KeyValuePair<EntityClass, List<Entity>> pair in entities)
-            {
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.Entities])
                 foreach (Entity e in pair.Value) e.Update(gt);
+
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.PollAdd])
+            {
+				while (pair.Value.Count > 0)
+				{
+					EntityCollections[Collections.Entities][pair.Key].Add(pair.Value.Last());
+					EntityCollections[Collections.PollAdd][pair.Key].Remove(pair.Value.Last());
+				}
             }
 
-            foreach (KeyValuePair<EntityClass, Queue<Entity>> pair in pollAddEntities)
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.PollRemove])
             {
-                while (pair.Value.Count > 0)
-                    entities[pair.Key].Add(pair.Value.Dequeue());
-            }
-
-            foreach (KeyValuePair<EntityClass, Queue<Entity>> pair in pollRemoveEntities)
-            {
-                while (pair.Value.Count > 0)
-                    entities[pair.Key].Remove(pair.Value.Dequeue());
+				while (pair.Value.Count > 0)
+				{
+					EntityCollections[Collections.Entities][pair.Key].Remove(pair.Value.Last());
+					EntityCollections[Collections.PollRemove][pair.Key].Remove(pair.Value.Last());
+				}
             }
         }
 
         public void Draw(GameTime gt)
         {
-            foreach (KeyValuePair<EntityClass, List<Entity>> pair in entities)
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.Entities])
             {
                 foreach (Entity e in pair.Value)
                     e.Draw(gt);
@@ -140,7 +120,7 @@ namespace Seihou
 
         public void ClearEntities()
         {
-            foreach (KeyValuePair<EntityClass, List<Entity>> pair in entities)
+            foreach (KeyValuePair<EntityClass, List<Entity>> pair in EntityCollections[Collections.Entities])
                 pair.Value.Clear();
         }
     }
