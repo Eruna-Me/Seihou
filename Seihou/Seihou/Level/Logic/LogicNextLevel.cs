f3117cd067389695d81d7445bb756de5e0d8c96a
Dennis <50838791+den-nis@users.noreply.github.com>
2022-03-14 13:43:10 -0700

    Added level change logic entity

---
+++ Seihou/Seihou/Level/Logic/LogicNextLevel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Seihou.Level.Logic
+{
+    internal class LogicNextLevel : LogicEntity
+    {
+        private readonly LevelManager _levelManager;
+        private readonly string _nextLevel;
+
+        public LogicNextLevel(LevelManager levelManager, EntityManager em, SpriteBatch sb, [Param("NextLevelName")] string nextLevelName) : base(em, sb, null)
+        {
+            _levelManager = levelManager;
+            _nextLevel = nextLevelName;
+        }
+
+        public override void OnSpawn()
+        {
+            _levelManager.LoadLevel(_nextLevel);
+        }
+    }
+}
