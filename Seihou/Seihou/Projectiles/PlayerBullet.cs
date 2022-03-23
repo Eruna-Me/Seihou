7a567bd20b4a7f97151dc922c6644dbc3b0cc601
Hidde Bartelink <hiddetelink@live.nl>
2022-03-23 14:01:54 +0100

    Added anti cheat
    no longer can hackers destroy enemies that haven't even entered the screen yet.

--- Seihou/Seihou/Projectiles/PlayerBullet.cs
+++ Seihou/Seihou/Projectiles/PlayerBullet.cs
@@ -3,14 +3,15 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Seihou
 {
-	class PlayerBullet : PlayerProjectile
-	{
-		public PlayerBullet(Vector2 pos, SpriteBatch sb, EntityManager em, Entity owner, Vector2 speed, string texture) : base(pos, sb, em, owner)
-		{
-			this.texture = texture;
-			ec = EntityManager.EntityClass.nonSolid;
-			size = 3;
-			this.speed = speed;
-		}
-	}
+    class PlayerBullet : PlayerProjectile
+    {
+        public PlayerBullet(Vector2 pos, SpriteBatch sb, EntityManager em, Entity owner, Vector2 speed, string texture) : base(pos, sb, em, owner)
+        {
+            this.texture = texture;
+            ec = EntityManager.EntityClass.nonSolid;
+
+            this.speed = speed;
+            outOfScreenMargin = ResourceManager.textures[texture].Height;
+        }
+    }
 }
--- Seihou/Seihou/Projectiles/PlayerProjectile.cs
+++ Seihou/Seihou/Projectiles/PlayerProjectile.cs
@@ -3,43 +3,45 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Seihou
 {
-	abstract class PlayerProjectile : Entity
-	{
+    abstract class PlayerProjectile : Entity
+    {
         protected readonly Entity owner;
+        protected int outOfScreenMargin;
 
-		protected PlayerProjectile(Vector2 pos, SpriteBatch sb, EntityManager em, Entity owner) : base(pos, sb, em)
-		{
+        protected PlayerProjectile(Vector2 pos, SpriteBatch sb, EntityManager em, Entity owner) : base(pos, sb, em)
+        {
             this.owner = owner;
-		}
+            outOfScreenMargin = -Global.outOfScreenMargin;
+        }
 
-		public override void Update(GameTime gt)
-		{
-			pos += speed * gt.Time();
+        public override void Update(GameTime gt)
+        {
+            pos += speed * gt.Time();
 
-			Entity c = em.Touching(this, EntityManager.EntityClass.enemy);
+            Entity c = em.Touching(this, EntityManager.EntityClass.enemy);
 
-			if (c != null)
-			{
-				if (c.hp > 0 && hp > 0)
-				{
-					hp--;
-					c.OnDamaged(owner, 1);
-					em.RemoveEntity(this);
-				}
-			}
+            if (c != null)
+            {
+                if (c.hp > 0 && hp > 0)
+                {
+                    hp--;
+                    c.OnDamaged(owner, 1);
+                    em.RemoveEntity(this);
+                }
+            }
 
-			if (pos.Y + Global.outOfScreenMargin < 0 || pos.Y > Global.screenHeight + Global.outOfScreenMargin || pos.X + Global.outOfScreenMargin < 0 || pos.X > Global.playingFieldWidth + Global.outOfScreenMargin)
-			{
-				em.RemoveEntity(this);
-			}
-		}
+            if (pos.Y + outOfScreenMargin < 0 || pos.Y > Global.screenHeight + outOfScreenMargin || pos.X + outOfScreenMargin < 0 || pos.X > Global.playingFieldWidth + outOfScreenMargin)
+            {
+                em.RemoveEntity(this);
+            }
+        }
 
-		public override void Draw(GameTime gt)
-		{
-			ResourceManager.DrawAngledTexture(sb, texture, pos, speed);
-			#if DRAWCOLBOX
+        public override void Draw(GameTime gt)
+        {
+            ResourceManager.DrawAngledTexture(sb, texture, pos, speed);
+#if DRAWCOLBOX
 			MonoGame.Primitives2D.DrawCircle(sb, pos, size, 10, Color.White, 1);
-			#endif
-		}
-	}
+#endif
+        }
+    }
 }
